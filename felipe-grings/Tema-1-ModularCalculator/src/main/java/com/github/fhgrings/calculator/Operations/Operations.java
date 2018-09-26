5bc9320f06698a8048a2b13fc75178e401003c82
Felipe Grings <felipe.grings@ilegra.com>
2018-09-26 09:38:01 -0300

    Remove extra fields

--- felipe-grings/Tema-1-ModularCalculator/src/main/java/com/github/fhgrings/calculator/Operations/Operations.java
+++
@@ -1,8 +0,0 @@
-package com.github.fhgrings.calculator.Operations;
-
-public interface Operations {
-
-    String calculate(double value1, double value2);
-
-    double resultTo4Decimals (double result);
-}
