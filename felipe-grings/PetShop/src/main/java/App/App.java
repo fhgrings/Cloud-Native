e93b469e6bc9b8eaca18dfeeb7fac9674e811ff1
Felipe Grings <felipe.grings@ilegra.com>
2018-09-26 09:44:31 -0300

    Fix variables names

--- felipe-grings/PetShop/src/main/java/App/App.java
+++ felipe-grings/PetShop/src/main/java/App/App.java
@@ -8,45 +8,45 @@ public class App {
 
     public static void main(String[] args) {
         Injector injector = Guice.createInjector(new GuiceModule());
-        PetShop petStore = injector.getInstance(PetShop.class);
+        PetShop petShop = injector.getInstance(PetShop.class);
 
-        petStore.addPet(1,"Rex","Pitbull");
-        petStore.addPet(2,"Frida","Rottweiler");
-        petStore.addPet(3,"Lara","German Sheperd");
-        petStore.addPet(4,"Tobi","NoN");
-        petStore.addPet(5,"Dark","Doberman");
-        petStore.addPet(6,"Pit","Bird");
-        petStore.addPet(7,"Bidu","Belga Sheperd");
-        petStore.addPet(8,"Toto","Eagle");
-        petStore.addPet(9,"Pirula","Pintcher");
-        petStore.addPet(10,"Tom","Dog");
-        petStore.addPet(11,"Kaka","Cat");
-        petStore.addPet(11,"Chico","Cat");
-        petStore.addPet(11,"Chico","Cat");
+        petShop.addPet(1,"Rex","Pitbull");
+        petShop.addPet(2,"Frida","Rottweiler");
+        petShop.addPet(3,"Lara","German Sheperd");
+        petShop.addPet(4,"Tobi","NoN");
+        petShop.addPet(5,"Dark","Doberman");
+        petShop.addPet(6,"Pit","Bird");
+        petShop.addPet(7,"Bidu","Belga Sheperd");
+        petShop.addPet(8,"Toto","Eagle");
+        petShop.addPet(9,"Pirula","Pintcher");
+        petShop.addPet(10,"Tom","Dog");
+        petShop.addPet(11,"Kaka","Cat");
+        petShop.addPet(11,"Chico","Cat");
+        petShop.addPet(11,"Chico","Cat");
 
         System.out.println("\n\n ====List Pets Names by ID====");
-        petStore.systemOptions(2,1);
-        petStore.systemOptions(2,2);
+        petShop.systemOptions(2,1);
+        petShop.systemOptions(2,2);
         System.out.println("\n");
 
-        petStore.systemOptions(1,2);
-        petStore.systemOptions(2,2);
+        petShop.systemOptions(1,2);
+        petShop.systemOptions(2,2);
 
         System.out.println("\n\n ===Services====");
 
-        petStore.service(1,true,1);
-        petStore.service(1,true,1);
-        petStore.service(1,true,1);
-        petStore.service(1,true,3);
-        petStore.service(2,false,4);
-        petStore.service(2,false,4);
-        petStore.service(3,true,3);
+        petShop.service(1,true,1);
+        petShop.service(1,true,1);
+        petShop.service(1,true,1);
+        petShop.service(1,true,3);
+        petShop.service(2,false,4);
+        petShop.service(2,false,4);
+        petShop.service(3,true,3);
 
         System.out.println("\n\n ====Historic====");
-        petStore.historic(1);
+        petShop.historic(1);
 
         System.out.println("\n\n ====Top 10 Pets Names====");
-        petStore.historic(2);
+        petShop.historic(2);
 
     }
 }
