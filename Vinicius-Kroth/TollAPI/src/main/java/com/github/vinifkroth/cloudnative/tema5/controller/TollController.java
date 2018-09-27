cc03969677ffceed281aaf5940e499c585146b9d
Vinicius Kroth <vinicius.kroth@ilegra.com>
2018-09-27 16:41:15 -0300

    First commit

---
+++ Vinicius-Kroth/TollAPI/src/main/java/com/github/vinifkroth/cloudnative/tema5/controller/TollController.java
@@ -0,0 +1,46 @@
+package com.github.vinifkroth.cloudnative.tema5.controller;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.stereotype.Controller;
+
+import com.github.vinifkroth.cloudnative.tema5.exception.InvalidVehicleException;
+import com.github.vinifkroth.cloudnative.tema5.exception.NotEnoughMoneyException;
+import com.github.vinifkroth.cloudnative.tema5.service.TollService;
+
+@Controller
+@Path("")
+public class TollController {
+
+	@Path("/ping")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String ping() {
+		return "pong1.4";
+	}
+
+	@Path("")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getPrices() {
+		return TollService.getPrices();
+	}
+
+	@Path("/charge")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String chargeFee(@DefaultValue("0") @QueryParam("vehicle") Integer vehicle,
+			@DefaultValue("0") @QueryParam("value") double money, @DefaultValue("0") @QueryParam("axis") int axis) {
+		try {
+			return "Fee paid, you may go along, and the change is $:" + TollService.chargeFee(vehicle, money, axis);
+		} catch (NotEnoughMoneyException | InvalidVehicleException e) {
+			return e.getMessage();
+		}
+	}
+
+}
