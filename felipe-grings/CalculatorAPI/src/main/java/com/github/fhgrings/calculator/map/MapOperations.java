02933b23ae079669c882ddf956b24c2875df73c2
Felipe Grings <felipe.grings@ilegra.com>
2018-09-28 16:02:53 -0300

    Add CalculatorAPI

---
+++ felipe-grings/CalculatorAPI/src/main/java/com/github/fhgrings/calculator/map/MapOperations.java
@@ -0,0 +1,12 @@
+package com.github.fhgrings.calculator.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapOperations {
+    public Map<String, Class> mapOperation = new HashMap<>();
+
+    public Map<String, Class> getMapOperation () {
+        return mapOperation;
+    }
+}
