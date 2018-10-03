0efff74c52cd486d3b4e33a336c24d3dbc9cb926
Vinicius Kroth <vinicius.kroth@ilegra.com>
2018-10-03 16:45:39 -0300

    Adding spring facet

--- Vinicius-Kroth/TollAPI/src/main/java/com/github/vinifkroth/cloudnative/tema5/service/TollService.java
+++ Vinicius-Kroth/TollAPI/src/main/java/com/github/vinifkroth/cloudnative/tema5/service/TollService.java
@@ -1,28 +1,40 @@
 package com.github.vinifkroth.cloudnative.tema5.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.github.vinifkroth.cloudnative.tema5.constants.TollPrices;
 import com.github.vinifkroth.cloudnative.tema5.exception.InvalidVehicleException;
 import com.github.vinifkroth.cloudnative.tema5.exception.NotEnoughMoneyException;
-import com.github.vinifkroth.cloudnative.tema5.singleton.PriceMapSingleton;
 
 public class TollService {
+	private static Map<Integer, Double> values;
 
-	public static String getPrices() {
+	public TollService() {
+		values = new HashMap<>();
+		values.put(1, TollPrices.BIKE);
+		values.put(2, TollPrices.MOTORCYCLE);
+		values.put(3, TollPrices.CAR);
+		values.put(4, TollPrices.BUS);
+		values.put(5, TollPrices.TRUCK);
+	}
+
+	public String getPrices() {
 		return "Prices:\n" + "1.Bycicles: " + TollPrices.BIKE + "\n2.Motorcycles: " + TollPrices.MOTORCYCLE
 				+ "\n3.Cars: " + TollPrices.CAR + "\n4.Bus: " + TollPrices.BUS + "\n5.Trucks: " + TollPrices.TRUCK
 				+ "\nExtra Truck Axis: " + TollPrices.EXTRA_AXIS;
 	}
 
-	public static double chargeFee(int vehicle, double money, int extraAxis)
+	public double chargeFee(int vehicle, double money, int extraAxis)
 			throws NotEnoughMoneyException, InvalidVehicleException {
 
-		Double price = PriceMapSingleton.getInstance().get(vehicle);
+		Double price = values.get(vehicle);
 
 		if (extraAxis > 0 && vehicle != 5)
 			throw new InvalidVehicleException("Invalid operation, only trucks can be charged with extra axis.");
 		if (price == null)
 			throw new InvalidVehicleException("The chosen option does not match any existing one in our database.");
-		
+
 		double change = money - (price + TollPrices.EXTRA_AXIS * extraAxis);
 
 		if (change < 0)
