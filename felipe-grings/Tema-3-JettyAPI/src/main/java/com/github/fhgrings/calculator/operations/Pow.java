1766e3f15df948aaa0ba8949bc2c27fd2c04a6e6
Felipe Grings <felipe.grings@ilegra.com>
2018-10-05 08:33:11 -0300

    Remove method call on constructor

--- felipe-grings/Tema-3-JettyAPI/src/main/java/com/github/fhgrings/calculator/operations/Pow.java
+++ felipe-grings/Tema-3-JettyAPI/src/main/java/com/github/fhgrings/calculator/operations/Pow.java
@@ -9,7 +9,6 @@ public class Pow implements Operations {
         this.value1 = value1;
         this.value2 = value2;
         roundResult = new ResultTo4Decimals();
-        calculate();
     }
 
     @Override
