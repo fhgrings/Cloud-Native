ad81f9eefd6584f4031a32d2bb3e8fb25da9740d
Rafael Ritter <rafael.ritter@ilegra.com>
2018-10-11 10:01:13 -0300

    Test v2.0

--- rafael-ritter/RxNetty/src/main/java/com/github/rafaritter44/cloud_native/rxnetty/AppRunner.java
+++ rafael-ritter/RxNetty/src/main/java/com/github/rafaritter44/cloud_native/rxnetty/AppRunner.java
@@ -1,24 +1,27 @@
 package com.github.rafaritter44.cloud_native.rxnetty;
 
-import com.github.rafaritter44.cloud_native.rxnetty.handler.RxNettyHandler;
-import com.github.rafaritter44.cloud_native.rxnetty.health.HealthCheck;
-
 import netflix.adminresources.resources.KaryonWebAdminModule;
 import netflix.karyon.Karyon;
 import netflix.karyon.KaryonBootstrapModule;
 import netflix.karyon.ShutdownModule;
 import netflix.karyon.archaius.ArchaiusBootstrapModule;
+import netflix.karyon.health.HealthCheckHandler;
 import netflix.karyon.servo.KaryonServoModule;
 import netflix.karyon.transport.http.health.HealthCheckEndpoint;
+import src.main.java.com.github.rafaritter44.cloud_native.rxnetty.handler.RxNettyHandler;
+import src.main.java.com.github.rafaritter44.cloud_native.rxnetty.health.HealthCheckResource;
 
 public class AppRunner {
 	public static void main(String[] args) {
-        HealthCheck healthCheckHandler = new HealthCheck();
+		System.setProperty("java.awt.headless","true");
+		System.setProperty("archaius.deployment.environment","dev");
+		
+		HealthCheckHandler healthCheckHandler = new HealthCheckResource();
         Karyon.forRequestHandler(8888,
                 new RxNettyHandler("/healthcheck",
                         new HealthCheckEndpoint(healthCheckHandler)),
                 new KaryonBootstrapModule(healthCheckHandler),
-                new ArchaiusBootstrapModule("hello-netflix-oss"),
+                new ArchaiusBootstrapModule("calculator-netflix-oss"),
                 Karyon.toBootstrapModule(KaryonWebAdminModule.class),
                 ShutdownModule.asBootstrapModule(),
                 KaryonServoModule.asBootstrapModule()
