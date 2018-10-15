065e80af6be710fb2d408ca314c7d2a460e5887e
Felipe Grings <felipe.grings@ilegra.com>
2018-10-15 13:29:47 -0300

    Remove Trash

--- felipe-grings/Tema-6-SpringBoot/Tema-6-IntegrationAPI/src/main/java/com/github/fhgrings/springboot/exception/FailedConnection.java
+++
@@ -1,8 +0,0 @@
-package com.github.fhgrings.springboot.exception;
-
-public class FailedConnection extends RuntimeException{
-    public FailedConnection(String message) {
-        super(message);
-    }
-
-}
