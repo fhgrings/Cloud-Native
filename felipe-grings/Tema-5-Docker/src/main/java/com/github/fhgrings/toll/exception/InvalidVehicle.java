5f71a42d659426e5a8e0451b89c33f45819b8241
Felipe Grings <felipe.grings@ilegra.com>
2018-10-16 08:23:45 -0300

    Test4

--- felipe-grings/Tema-5-Docker/src/main/java/com/github/fhgrings/toll/exception/InvalidVehicle.java
+++
@@ -1,8 +0,0 @@
-package com.github.fhgrings.toll.exception;
-
-public class InvalidVehicle extends RuntimeException {
-
-    public InvalidVehicle(String message) {
-        super(message);
-    }
-}
--- felipe-grings/Tema-5-Docker/src/main/java/com/github/fhgrings/toll/exception/NotEnoughMoney.java
+++
@@ -1,7 +0,0 @@
-package com.github.fhgrings.toll.exception;
-
-public class NotEnoughMoney extends RuntimeException{
-    public NotEnoughMoney(String message) {
-        super(message);
-    }
-}
