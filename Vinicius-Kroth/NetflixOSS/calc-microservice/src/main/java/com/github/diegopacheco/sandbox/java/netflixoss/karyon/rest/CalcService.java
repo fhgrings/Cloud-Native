cf1fce09e6d554cb233cbf6f7d060f1d53c627bb
Vinicius Kroth <vinicius.kroth@ilegra.com>
2018-10-18 15:23:26 -0300

    Add pow microservice

--- Vinicius-Kroth/NetflixOSS/calc-microservice/src/main/java/com/github/diegopacheco/sandbox/java/netflixoss/karyon/rest/CalcService.java
+++ Vinicius-Kroth/NetflixOSS/calc-microservice/src/main/java/com/github/diegopacheco/sandbox/java/netflixoss/karyon/rest/CalcService.java
@@ -9,59 +9,65 @@ import com.github.diegopacheco.sandbox.java.netflixoss.karyon.ribbon.RibbonMathC
 
 @Singleton
 public class CalcService {
-	
+
 	private RibbonMathClient client;
-	
+
 	@Inject
 	public CalcService(RibbonMathClient client) {
 		this.client = client;
 	}
-	
-	public Double calc(String expr){
-		
-		if ( expr==null || ("".equals(expr)) ) 
+
+	public Double calc(String expr) {
+
+		if (expr == null || ("".equals(expr)))
 			throw new IllegalArgumentException("You must pass a valid expression. ");
-		
+
 		Stack<String> stack = new Stack<String>();
 		String s1;
 		Double a;
 		Double b;
 		Double result = 0.0;
-		
-		for (int i = 0; i < expr.length(); i++){
-		     s1 = expr.charAt(i) + "";
-		     
-		     if (" ".equals(s1)) continue;
-		     
-		     if(s1.equals("+") || s1.equals("-") || s1.equals("*") || s1.equals("/")){
-		          switch (s1){
-		                case "+":
-		                    a = new Double(stack.pop());
-		                    b = new Double(stack.pop());
-		                    result = client.sum(a, b).toBlocking().first();
-		                    break;
-		                case "-":
-		                	a = new Double(stack.pop());
-		                    b = new Double(stack.pop());
-		                    result = client.sub(a, b).toBlocking().first();
-		                    break;
-		                case "/":
-		                	a = new Double(stack.pop());
-		                    b = new Double(stack.pop());
-		                    result = client.div(a, b).toBlocking().first();
-		                    break;
-		                case "*":
-		                	a = new Double(stack.pop());
-		                    b = new Double(stack.pop());
-		                    result = client.mul(a, b).toBlocking().first();
-		                    break;
-		                }
-		         }else{
-		             stack.push(s1);
-		         }
+
+		for (int i = 0; i < expr.length(); i++) {
+			s1 = expr.charAt(i) + "";
+
+			if (" ".equals(s1))
+				continue;
+
+			if (s1.equals("+") || s1.equals("-") || s1.equals("*") || s1.equals("/") || s1.equals("^")) {
+				switch (s1) {
+				case "+":
+					a = new Double(stack.pop());
+					b = new Double(stack.pop());
+					result = client.sum(a, b).toBlocking().first();
+					break;
+				case "-":
+					a = new Double(stack.pop());
+					b = new Double(stack.pop());
+					result = client.sub(b, a).toBlocking().first();
+					break;
+				case "/":
+					a = new Double(stack.pop());
+					b = new Double(stack.pop());
+					result = client.div(b, a).toBlocking().first();
+					break;
+				case "*":
+					a = new Double(stack.pop());
+					b = new Double(stack.pop());
+					result = client.mul(a, b).toBlocking().first();
+					break;
+				case "^":
+					a = new Double(stack.pop());
+					b = new Double(stack.pop());
+					result = client.pow(b, a).toBlocking().first();
+					break;
+				}
+			} else {
+				stack.push(s1);
+			}
 		}
 		return result;
-		
+
 	}
-	
+
 }
---
+++ Vinicius-Kroth/NetflixOSS/pow-microservice/src/main/java/com/github/vinifkroth/cloudnative/tema9/karyon/ShutdownModule.java
@@ -0,0 +1,10 @@
+package com.github.vinifkroth.cloudnative.tema9.karyon;
+
+
+public class ShutdownModule extends netflix.karyon.ShutdownModule{
+	
+	public ShutdownModule() {
+	    super(7007);
+	}
+	
+}
