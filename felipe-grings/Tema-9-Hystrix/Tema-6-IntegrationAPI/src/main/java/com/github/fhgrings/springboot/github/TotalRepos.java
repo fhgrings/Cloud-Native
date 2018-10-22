d552016c2daf9651efd069a6ba4e94841b28d8d8
Felipe Grings <felipe.grings@ilegra.com>
2018-10-22 10:28:26 -0300

    Add Hystrix

--- felipe-grings/Tema-9-Hystrix/Tema-6-IntegrationAPI/src/main/java/com/github/fhgrings/springboot/github/TotalRepos.java
+++ felipe-grings/Tema-9-Hystrix/Tema-6-IntegrationAPI/src/main/java/com/github/fhgrings/springboot/github/TotalRepos.java
@@ -1,12 +1,19 @@
 package com.github.fhgrings.springboot.github;
 
 import com.github.fhgrings.springboot.exception.FailedConnection;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+
+@Service
 public class TotalRepos {
 
     private RestTemplate restTemplate = new RestTemplate();
 
+
+    @HystrixCommand(fallbackMethod = "defaultRepos", commandProperties = @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000"))
     public int execute(String user) throws FailedConnection {
         String url = "http://172.18.0.25:8080/repos?user=";
 
@@ -16,5 +23,9 @@ public class TotalRepos {
             throw new FailedConnection("Error: Connection Failed");
         }
 
+    }
+
+    public int defaultRepos(String username) {
+        return 0;
     }
 }
--- felipe-grings/Tema-9-Hystrix/Tema-6-IntegrationAPI/src/main/java/com/github/fhgrings/springboot/twitter/TotalTweets.java
+++ felipe-grings/Tema-9-Hystrix/Tema-6-IntegrationAPI/src/main/java/com/github/fhgrings/springboot/twitter/TotalTweets.java
@@ -1,12 +1,17 @@
 package com.github.fhgrings.springboot.twitter;
 
 import com.github.fhgrings.springboot.exception.FailedConnection;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class TotalTweets {
 
     private RestTemplate restTemplate = new RestTemplate();
 
+    @HystrixCommand(fallbackMethod = "defaultRepos", commandProperties = @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000"))
     public int execute(String user) throws FailedConnection {
         String url = "http://172.18.0.23:8080/tweet?user=";
 
@@ -16,6 +21,10 @@ public class TotalTweets {
         } catch (Exception e) {
             throw new FailedConnection("Error: Connection Failed");
         }
+    }
+
+    public int defaultRepos(String username) {
+        return 0;
     }
 
 }
